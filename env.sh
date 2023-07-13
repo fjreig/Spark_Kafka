1262d94973e913caf9541ed8f2733aece96ac000
FJ Reig <48714328+fjreig@users.noreply.github.com>
2023-07-13 17:17:46 +0200

    Delete env.sh

--- env.sh
+++
@@ -1,4 +0,0 @@
-spark.master                     spark://spark-master:7077
-spark.jars.packages              spark-cassandra-connector_2.12-3.3.0 
-spark.cassandra.connection.host  cassandra
-spark.driver.host                spark-master
